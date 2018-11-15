53f650b1e2d336190229dff176d1b5f31d1120f0
Bruce <tseng1985@gmail.com>
2018-11-15 11:23:31 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app" class="pt-3 pb-3">
+    <input-form :ttl="title" />
   </div>
 </template>
+
+<script>
+// inputFormをComponentとして設定
+import InputForm from '@/components/InputForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    InputForm
+  },
+  data() {
+    return {
+      // titleをpropとしてComponentに渡す
+      title : "勤怠連絡 BOT"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
