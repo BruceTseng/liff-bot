d42f9e3ff282dbd93ea084d11bbe604d60d8a344
Bruce <tseng1985@gmail.com>
2018-11-15 12:07:24 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <about></about>
   </div>
 </template>
+<script>
+import About from '@/views/About.vue';
+export default {
+  components: {About}
+}
+</script>
 
 <style lang="scss">
 #app {
